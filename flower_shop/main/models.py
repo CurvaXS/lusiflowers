4ebc092cbad9e07cd4553cbec428751a3aacf8b6
CurvaXs <suxarik24@gmail.com>
2024-06-02 00:26:12 +0300

    0.2v

--- flower_shop/main/models.py
+++ flower_shop/main/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+TYPE_CHOISES = {
+    ('AutorsBucket', 'AutorsBucket'),
+    ('Compositions', 'Compositions'),
+    ('Busket', 'Busket'),
+}
+
+class AutorsBucket(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    
+    class Meta:
+        db_table = 'autors_buckets'
+
+class Compositions(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    
+    class Meta:
+        db_table = 'compositions'
+
+class Busket(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    
+    class Meta:
+        db_table = 'baskets'
+
+class Flower(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    choise = models.CharField(max_length=100, choices=TYPE_CHOISES)
+    
+    
+    class Meta:
+        db_table = 'flowers'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Логика для дублирования объекта Flower в одну из таблиц
+        if self.choise == 'AutorsBucket':  # Например, условие для добавления в AutorsBucket
+            bucket = AutorsBucket.objects.first()
+            if bucket:
+                bucket.flowers.add(self)
+            else:
+                AutorsBucket.objects.create(name=self.name, price=self.price)
+        elif self.choise == 'Compositions':  # Условие для добавления в Compositions
+            composition = Compositions.objects.first()
+            if composition:
+                composition.flowers.add(self)
+            else:
+                Compositions.objects.create(name=self.name, price=self.price)
+        else:  # Условие для добавления в Busket
+            basket = Busket.objects.first()
+            if basket:
+                basket.flowers.add(self)
+            else:
+                Busket.objects.create(name=self.name, price=self.price)
--- flower_shop/main/views.py
+++ flower_shop/main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    autors_bucket = AutorsBucket.objects.all()
+    compositions = Compositions.objects.all()
+    busket = Busket.objects.all()
+    return render(request, 'main/index.html', {
+        'autors_bucket': autors_bucket,
+        'compositions': compositions,
+        'busket': busket
+        })
