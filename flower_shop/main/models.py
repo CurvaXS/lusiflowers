dd4a3ce1257966c2ad0ba0ccebf0e65b4aea2e39
CurvaXs <suxarik24@gmail.com>
2024-06-08 14:54:06 +0300

    0.2.1v

--- flower_shop/main/models.py
+++ flower_shop/main/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
-
-TYPE_CHOISES = {
+TYPE_CHOICES = (
     ('AutorsBucket', 'AutorsBucket'),
     ('Compositions', 'Compositions'),
     ('Busket', 'Busket'),
-}
+)
 
 class AutorsBucket(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    photo_url = models.CharField(max_length=9999, default='')
+    flowers = models.ManyToManyField('Flower', related_name='autors_buckets', blank=True)
     
     class Meta:
         db_table = 'autors_buckets'
@@ -17,6 +18,8 @@ class AutorsBucket(models.Model):
 class Compositions(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    photo_url = models.URLField(blank=True, null=True)
+    flowers = models.ManyToManyField('Flower', related_name='compositions', blank=True)
     
     class Meta:
         db_table = 'compositions'
@@ -24,6 +27,8 @@ class Compositions(models.Model):
 class Busket(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    photo_url = models.URLField(blank=True, null=True)
+    flowers = models.ManyToManyField('Flower', related_name='baskets', blank=True)
     
     class Meta:
         db_table = 'baskets'
@@ -31,30 +36,34 @@ class Busket(models.Model):
 class Flower(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
-    choise = models.CharField(max_length=100, choices=TYPE_CHOISES)
-    
+    photo = models.ImageField(upload_to="photos", default=None, blank=True, null=True, verbose_name="Фото")
+    choise = models.CharField(max_length=100, choices=TYPE_CHOICES)
     
     class Meta:
         db_table = 'flowers'
     
+    @property
+    def photo_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+        
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        # Логика для дублирования объекта Flower в одну из таблиц
-        if self.choise == 'AutorsBucket':  # Например, условие для добавления в AutorsBucket
+        if self.choise == 'AutorsBucket':
             bucket = AutorsBucket.objects.first()
             if bucket:
                 bucket.flowers.add(self)
             else:
-                AutorsBucket.objects.create(name=self.name, price=self.price)
-        elif self.choise == 'Compositions':  # Условие для добавления в Compositions
+                AutorsBucket.objects.create(name=self.name, price=self.price, photo_url=self.photo_url)
+        elif self.choise == 'Compositions':
             composition = Compositions.objects.first()
             if composition:
                 composition.flowers.add(self)
             else:
-                Compositions.objects.create(name=self.name, price=self.price)
-        else:  # Условие для добавления в Busket
+                Compositions.objects.create(name=self.name, price=self.price, photo_url=self.photo_url)
+        else:
             basket = Busket.objects.first()
             if basket:
                 basket.flowers.add(self)
             else:
-                Busket.objects.create(name=self.name, price=self.price)
+                Busket.objects.create(name=self.name, price=self.price, photo_url=self.photo_url)
